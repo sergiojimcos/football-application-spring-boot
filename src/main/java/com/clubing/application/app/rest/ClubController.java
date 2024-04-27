e5e8ac612e3a19c22b7ee18c89371cbb837410f7
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-04-27 17:13:35 +0200

    fix: Make sure that always must consume an application/json

--- src/main/java/com/clubing/application/app/rest/ClubController.java
+++ src/main/java/com/clubing/application/app/rest/ClubController.java
@@ -4,6 +4,7 @@ package com.clubing.application.app.rest;
 import com.clubing.application.app.rest.api.dto.ClubDTO;
 import com.clubing.application.app.rest.api.dto.PlayerDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,35 +15,35 @@ import java.util.List;
 @RequestMapping("/club")
 public class ClubController {
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ClubDTO> postClub(@RequestBody ClubDTO clubDTO) {
 
         return null;
     }
 
-    @PostMapping("/{clubId}/player")
+    @PostMapping(value = "/{clubId}/player", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> postClubWithPlayer(@PathVariable Long clubId, @RequestBody PlayerDTO playerDTO) {
 
         return null;
     }
 
-    @GetMapping
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ClubDTO>> getClubs() {
         return null;
     }
 
-    @GetMapping("/{clubId}")
+    @GetMapping(value ="/{clubId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getClubById(@PathVariable Long clubId) {
         return null;
     }
 
-    @GetMapping("/{clubId}/player")
+    @GetMapping(value = "/{clubId}/player", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PlayerDTO>> getAllPlayersByClubId(@PathVariable Long clubId) {
 
         return null;
     }
 
-    @GetMapping("/{clubId}/player/{playerId}")
+    @GetMapping(value = "/{clubId}/player/{playerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getPlayerById(@PathVariable Long clubId, @PathVariable Long playerId) {
 
 
