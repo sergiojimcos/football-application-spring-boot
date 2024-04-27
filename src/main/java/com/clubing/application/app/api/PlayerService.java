deb77768589cf88a897d66733a2ede4eef406fdc
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-04-27 18:39:05 +0200

    feat: Create PlayerService interface

---
+++ src/main/java/com/clubing/application/app/api/PlayerService.java
@@ -0,0 +1,19 @@
+package com.clubing.application.app.api;
+
+import com.clubing.application.app.service.model.PlayerEntry;
+
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+public interface PlayerService {
+    public PlayerEntry addPlayerEntry(long clubId, String name, String surname, String nationality, String email,
+                                 Date birthDate) throws Exception;
+
+    public PlayerEntry getPlayerEntry(long playerId) throws Exception;
+
+    public Collection<PlayerEntry> getPlayerEntriesByClubId(long clubId) throws Exception;
+    
+}
