6a92fea62a043c5f1c189212a6e13b410b0a1b42
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-04-28 12:16:15 +0200

    feat: Create TokenDTO for better practice

---
+++ src/main/java/com/clubing/application/app/rest/api/dto/TokenDTO.java
@@ -0,0 +1,24 @@
+package com.clubing.application.app.rest.api.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
+public class TokenDTO implements Serializable {
+
+    private String accessToken;
+
+    public TokenDTO(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
