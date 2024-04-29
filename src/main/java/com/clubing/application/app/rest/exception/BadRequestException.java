937903220c6dfd3deae98660df57ce42267366fe
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-04-29 22:58:42 +0200

    fix: Add author header

--- src/main/java/com/clubing/application/app/rest/exception/BadRequestException.java
+++ src/main/java/com/clubing/application/app/rest/exception/BadRequestException.java
@@ -3,6 +3,10 @@ package com.clubing.application.app.rest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
--- src/main/java/com/clubing/application/app/rest/exception/UnauthorizedException.java
+++ src/main/java/com/clubing/application/app/rest/exception/UnauthorizedException.java
@@ -3,6 +3,10 @@ package com.clubing.application.app.rest.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
