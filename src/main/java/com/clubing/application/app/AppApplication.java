4fcacb20b0fdc044d745b3b61882072cd6360012
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-04-29 22:07:08 +0200

    feat: Add all the dependencies for JPA

--- src/main/java/com/clubing/application/app/AppApplication.java
+++ src/main/java/com/clubing/application/app/AppApplication.java
@@ -2,8 +2,12 @@ package com.clubing.application.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.clubing.application.app.service.model")
+@EnableJpaRepositories(basePackages = "com.clubing.application.app.service.repository")
 public class AppApplication {
 
 	public static void main(String[] args) {
