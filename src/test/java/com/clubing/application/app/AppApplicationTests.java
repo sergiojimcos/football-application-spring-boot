2683959463dda29398ead87a5c9dcff4a4e1a519
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-05-01 10:26:46 +0200

    fix: Refactor tests

--- src/test/java/com/clubing/application/app/AppApplicationTests.java
+++ src/test/java/com/clubing/application/app/AppApplicationTests.java
@@ -23,118 +23,107 @@ import static org.assertj.core.api.BDDAssertions.then;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppApplicationTests {
 
-	private static String _BASE_POST_CLUB;
+    private static String _BASE_POST_CLUB;
 
-	private static String _BASE_POST_PLAYER;
+    private static String _BASE_POST_PLAYER;
 
-	private static String _BASE_LOGGING_USER;
+    private static String _BASE_LOGGING_USER;
 
-	private String _token;
+    private String _token;
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	private TestRestTemplate testRestTemplate;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-	@Before
-	public void setUp() {
-		_BASE_LOGGING_USER = "http://localhost:" + this.port + "/login";
-		_BASE_POST_CLUB = "http://localhost:" + this.port + "/club";
-		_BASE_POST_PLAYER = "http://localhost:" + this.port + "/club/%s/player";
+    @Before
+    public void setUp() {
+        _BASE_LOGGING_USER = "http://localhost:" + this.port + "/login";
+        _BASE_POST_CLUB = "http://localhost:" + this.port + "/club";
+        _BASE_POST_PLAYER = "http://localhost:" + this.port + "/club/%s/player";
 
-		ResponseEntity<TokenDTO> tokenEntity = this.testRestTemplate.postForEntity(_BASE_LOGGING_USER,
-				new UserDTO(RandomStringUtils.random(5), "password123_"), TokenDTO.class);
+        ResponseEntity<TokenDTO> tokenEntity = this.testRestTemplate.postForEntity(_BASE_LOGGING_USER,
+                new UserDTO(RandomStringUtils.random(5), "password123_"), TokenDTO.class);
 
-		_token = tokenEntity.getBody().getAccessToken();
+        _token = tokenEntity.getBody().getAccessToken();
 
-	}
+    }
 
-	@Test
-	public void testCreateValidClub() throws Exception {
-		ClubDTO clubDTO = new ClubDTO( RandomStringUtils.random(4) + "@email.com","password", "official", "popularName", "federation", true);
+    @Test
+    public void testCreateValidClub() throws Exception {
+        ClubDTO clubDTO = new ClubDTO(RandomStringUtils.random(4) + "@email.com", "password", "official", "popularName", "federation", true);
 
-		HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
 
-		headers.setBearerAuth(_token);
+        headers.setBearerAuth(_token);
 
-		HttpEntity<ClubDTO> httpEntity = new HttpEntity<>(clubDTO, headers);
+        HttpEntity<ClubDTO> httpEntity = new HttpEntity<>(clubDTO, headers);
 
-		ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB, HttpMethod.POST, httpEntity, ClubDTO.class);
+        ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB, HttpMethod.POST, httpEntity, ClubDTO.class);
 
-		_assertValidClub(clubDTOResponseEntity, clubDTO);
-	}
+        _assertValidClub(clubDTOResponseEntity, clubDTO);
+    }
 
-	@Test
-	public void testGetClubById() throws Exception {
-		ClubDTO clubDTO = _postClub(new ClubDTO(RandomStringUtils.random(4) + "@email.com", "password",
-				"official", "popularName", "federation", true));
+    @Test
+    public void testGetClubById() throws Exception {
+        ClubDTO clubDTO = _postClub(new ClubDTO(RandomStringUtils.random(4) + "@email.com", "password",
+                "official", "popularName", "federation", true));
 
-		HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB + "/" +
+                clubDTO.getClubId(), HttpMethod.GET, _buildHttpEntity(clubDTO), ClubDTO.class);
 
-		headers.setBearerAuth(_token);
+        _assertValidClub(clubDTOResponseEntity, clubDTO);
+    }
 
-		HttpEntity<ClubDTO> httpEntity = new HttpEntity<>(clubDTO, headers);
+    @Test
+    public void testGetClubByIdWithSeveralPlayers() throws Exception {
+        ClubDTO clubDTO = _postClub(new ClubDTO("email@email.com", "password",
+                "official", "popularName", "federation", true));
 
-		ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB + "/" +
-				clubDTO.getClubId(), HttpMethod.GET, httpEntity, ClubDTO.class);
+        _postPlayer(new PlayerDTO("Aritz", "Aduriz", "Spanish", "arit@bilbao.com",
+                new Date()), clubDTO.getClubId());
 
-		_assertValidClub(clubDTOResponseEntity, clubDTO);
-	}
+        clubDTO.setTotalPlayers(1);
 
-	@Test
-	public void testGetClubByIdWithSeveralPlayers() throws Exception {
-		ClubDTO clubDTO = _postClub(new ClubDTO("email@email.com", "password",
-				"official", "popularName", "federation", true));
+        ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB + "/" +
+                clubDTO.getClubId(), HttpMethod.GET, _buildHttpEntity(clubDTO), ClubDTO.class);
 
-		_postPlayer(new PlayerDTO("Aritz", "Aduriz", "Spanish", "arit@bilbao.com",
-				new Date()), clubDTO.getClubId());
+        _assertValidClub(clubDTOResponseEntity, clubDTO);
+    }
 
-		clubDTO.setTotalPlayers(1);
+    private void _assertValidClub(ResponseEntity<ClubDTO> actualClubDTO, ClubDTO expectedClubDTO) {
+        then(actualClubDTO.getBody().getUserName()).isEqualTo(expectedClubDTO.getUserName());
+        then(actualClubDTO.getBody().getPassword()).isEqualTo(expectedClubDTO.getPassword());
+        then(actualClubDTO.getBody().getFederation()).isEqualTo(expectedClubDTO.getFederation());
+        then(actualClubDTO.getBody().getOfficialName()).isEqualTo(expectedClubDTO.getOfficialName());
+        then(actualClubDTO.getBody().getPopularName()).isEqualTo(expectedClubDTO.getPopularName());
+        then(actualClubDTO.getBody().isPublic()).isEqualTo(expectedClubDTO.isPublic());
+    }
 
-		HttpHeaders headers = new HttpHeaders();
+    private <T> HttpEntity<T> _buildHttpEntity(T dtoEntity) {
+        HttpHeaders headers = new HttpHeaders();
 
-		headers.setBearerAuth(_token);
+        headers.setBearerAuth(_token);
 
-		HttpEntity<ClubDTO> httpEntity = new HttpEntity<>(clubDTO, headers);
+        HttpEntity<T> httpEntity = new HttpEntity<>(dtoEntity, headers);
 
-		ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB + "/" +
-				clubDTO.getClubId(), HttpMethod.GET, httpEntity, ClubDTO.class);
+        return httpEntity;
+    }
 
-		_assertValidClub(clubDTOResponseEntity, clubDTO);
-	}
+    private ClubDTO _postClub(ClubDTO clubDTO) {
 
-	private void _assertValidClub(ResponseEntity<ClubDTO> actualClubDTO, ClubDTO expectedClubDTO) {
-		then(actualClubDTO.getBody().getUserName()).isEqualTo(expectedClubDTO.getUserName());
-		then(actualClubDTO.getBody().getPassword()).isEqualTo(expectedClubDTO.getPassword());
-		then(actualClubDTO.getBody().getFederation()).isEqualTo(expectedClubDTO.getFederation());
-		then(actualClubDTO.getBody().getOfficialName()).isEqualTo(expectedClubDTO.getOfficialName());
-		then(actualClubDTO.getBody().getPopularName()).isEqualTo(expectedClubDTO.getPopularName());
-		then(actualClubDTO.getBody().isPublic()).isEqualTo(expectedClubDTO.isPublic());
-	}
+        ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(
+                _BASE_POST_CLUB, HttpMethod.POST, _buildHttpEntity(clubDTO), ClubDTO.class);
 
-	private ClubDTO _postClub(ClubDTO clubDTO) {
+        return clubDTOResponseEntity.getBody();
+    }
 
-		HttpHeaders headers = new HttpHeaders();
+    private PlayerDTO _postPlayer(PlayerDTO playerDTO, long clubId) {
 
-		headers.setBearerAuth(_token);
+        ResponseEntity<PlayerDTO> playerDTOResponseEntity = this.testRestTemplate.exchange(
+                String.format(_BASE_POST_PLAYER, clubId), HttpMethod.POST, _buildHttpEntity(playerDTO), PlayerDTO.class);
 
-		HttpEntity<ClubDTO> httpEntity = new HttpEntity<>(clubDTO, headers);
-
-		ResponseEntity<ClubDTO> clubDTOResponseEntity = this.testRestTemplate.exchange(_BASE_POST_CLUB, HttpMethod.POST, httpEntity, ClubDTO.class);
-
-		return clubDTOResponseEntity.getBody();
-	}
-
-	private PlayerDTO _postPlayer(PlayerDTO playerDTO, long clubId){
-		HttpHeaders headers = new HttpHeaders();
-
-		headers.setBearerAuth(_token);
-
-		HttpEntity<PlayerDTO> httpEntity = new HttpEntity<>(playerDTO, headers);
-
-		ResponseEntity<PlayerDTO> playerDTOResponseEntity = this.testRestTemplate.exchange(String.format(_BASE_POST_PLAYER, clubId), HttpMethod.POST, httpEntity, PlayerDTO.class);
-
-		return playerDTOResponseEntity.getBody();
-	}
+        return playerDTOResponseEntity.getBody();
+    }
 }
