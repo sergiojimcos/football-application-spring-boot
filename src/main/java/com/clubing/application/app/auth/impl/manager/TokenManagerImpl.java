00eefeaa3d3e7bb8d4feecddc70c7e9827e7ac5b
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-05-02 17:48:06 +0200

    fix: Remove unnecesary imports

--- src/main/java/com/clubing/application/app/auth/impl/manager/TokenManagerImpl.java
+++ src/main/java/com/clubing/application/app/auth/impl/manager/TokenManagerImpl.java
@@ -7,8 +7,6 @@ import com.clubing.application.app.service.model.UserEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 /**
