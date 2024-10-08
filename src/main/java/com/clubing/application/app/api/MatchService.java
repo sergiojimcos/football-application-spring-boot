6ebe4b682938271e3f82524eb7913e53ed129371
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-10-08 19:45:25 +0200

    feat: Create new method in order to create new matches

--- src/main/java/com/clubing/application/app/api/MatchService.java
+++ src/main/java/com/clubing/application/app/api/MatchService.java
@@ -1,4 +1,14 @@
 package com.clubing.application.app.api;
 
+import com.clubing.application.app.service.model.Match;
+
+import java.util.Date;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
 public interface MatchService {
+
+    public Match addMatch(Date matchDate, long localClubId, long visitantClubId);
 }
--- src/main/java/com/clubing/application/app/service/impl/MatchServiceImpl.java
+++ src/main/java/com/clubing/application/app/service/impl/MatchServiceImpl.java
@@ -1,4 +1,47 @@
 package com.clubing.application.app.service.impl;
 
-public class MatchServiceImpl {
+import com.clubing.application.app.api.ClubService;
+import com.clubing.application.app.api.MatchService;
+import com.clubing.application.app.rest.impl.exception.NotFoundException;
+import com.clubing.application.app.service.model.ClubEntry;
+import com.clubing.application.app.service.model.Match;
+import com.clubing.application.app.service.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
+@Service
+public class MatchServiceImpl implements MatchService {
+
+    @Autowired
+    private ClubService clubService;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+
+    @Override
+    public Match addMatch(Date matchDate, long localClubId, long visitantClubId) {
+
+        ClubEntry localClubEntry = clubService.fetchClubEntry(localClubId);
+        ClubEntry visitantClubEntry = clubService.fetchClubEntry(visitantClubId);
+
+        if (localClubEntry == null) {
+            throw new NotFoundException("No local club with id: " + localClubId + " is found");
+        }
+
+        if (visitantClubEntry == null) {
+            throw new NotFoundException("No visitant club with id: " + localClubId + " is found");
+        }
+
+        Match match = new Match(new Time(matchDate.getTime()), localClubEntry, visitantClubEntry);
+
+        return matchRepository.saveAndFlush(match);
+    }
 }
