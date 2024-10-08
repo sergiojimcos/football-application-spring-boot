497a41cb4486f84abe90b790a33b374fae2e18a1
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-10-08 18:53:31 +0200

    feat: Add MatchDTO properties

--- src/main/java/com/clubing/application/app/rest/api/dto/MatchDTO.java
+++ src/main/java/com/clubing/application/app/rest/api/dto/MatchDTO.java
@@ -1,4 +1,20 @@
 package com.clubing.application.app.rest.api.dto;
 
-public class MatchDto {
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
+
+public class MatchDTO implements Serializable {
+
+    private Long id;
+
+    private Date matchDate;
+
+    private Long localClubId;
+
+    private Long visitantClubId;
 }
