8aee5399c8f73b9c3a1d3bd0381c85aa781e6d65
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-10-28 19:36:20 +0100

    feat: Add new Converter implementation

--- src/main/java/com/clubing/application/app/rest/impl/converter/TokenDTOConverter.java
+++ src/main/java/com/clubing/application/app/rest/impl/converter/TokenDTOConverter.java
@@ -1,14 +1,19 @@
 package com.clubing.application.app.rest.impl.converter;
 
 import com.clubing.application.app.rest.api.dto.TokenDTO;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Sergio Jiménez del Coso
  */
 
-public class TokenDTOConverter {
+@Component
+public class TokenDTOConverter implements Converter<String, TokenDTO> {
 
-    public static TokenDTO toDTO(String token) {
+
+    @Override
+    public TokenDTO convert(String token) {
         return new TokenDTO(token);
     }
 }
