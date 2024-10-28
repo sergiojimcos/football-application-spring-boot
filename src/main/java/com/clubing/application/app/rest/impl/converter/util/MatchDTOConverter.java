d08e13b3c841bbaf5be14bacf162daaf7f993f65
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-10-28 19:14:22 +0100

    feat: Rename and implement MatchDTOConverter

---
+++ src/main/java/com/clubing/application/app/rest/impl/converter/util/MatchDTOConverter.java
@@ -0,0 +1,20 @@
+package com.clubing.application.app.rest.impl.converter.util;
+
+import com.clubing.application.app.rest.api.dto.MatchOutDTO;
+import com.clubing.application.app.service.model.Match;
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Date;
+
+public class MatchDTOConverter implements Converter<Match, MatchOutDTO> {
+
+    @Override
+    public MatchOutDTO convert(Match match) {
+
+        return new MatchOutDTO(){{
+            setMatchDate(new Date(match.getMatchDate().getTime()));
+            setLocalClub(ClubDTOConverterUtil.toDTO(match.getLocalClubEntry()));
+            setVisitantClub(ClubDTOConverterUtil.toDTO(match.getVisitantClubEntry()));
+        }};
+    }
+}
--- src/main/java/com/clubing/application/app/rest/impl/converter/util/MatchDTOConverterUtil.java
+++
@@ -1,4 +0,0 @@
-package com.clubing.application.app.rest.impl.converter.util;
-
-public class MatchDTOConverterUtil {
-}
