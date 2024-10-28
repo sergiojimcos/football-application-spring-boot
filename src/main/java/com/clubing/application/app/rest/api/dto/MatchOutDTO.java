4244537661bdd6ca5fe41281c9d509bca8c5f56b
Sergio Jiménez del Coso <sjc1b@hotmail.es>
2024-10-28 19:13:36 +0100

    feat: Add properties to MatchOutDTO

--- src/main/java/com/clubing/application/app/rest/api/dto/MatchOutDTO.java
+++ src/main/java/com/clubing/application/app/rest/api/dto/MatchOutDTO.java
@@ -1,4 +1,28 @@
 package com.clubing.application.app.rest.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author Sergio Jiménez del Coso
+ */
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
 public class MatchOutDTO {
+
+    private Date matchDate;
+
+    private ClubDTO localClub;
+
+    private ClubDTO visitantClub;
 }
